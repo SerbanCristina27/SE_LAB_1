3a38d27b39d622bd4846b60ce98b35b2efc1f273
SerbanCristina <anp.cristina.serban@gmail.com>
2016-10-17 23:11:03 +0300

    Primul cod de la laborator!

---
+++ src/ro/mta/se/lab1/Main.java
@@ -0,0 +1,11 @@
+package ro.mta.se.lab1;
+
+/**
+ * Created by User on 10/17/2016.
+ */
+public class Main {
+    public static void main(String args[]) {
+        int array[] = {1, 2, 3};
+        System.out.println("Primul laborator "+ array[1]);
+    }
+}
